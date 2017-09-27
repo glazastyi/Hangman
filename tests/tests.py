b6f95f29b204ef9a9ece168fa5b66c889e552524
Nikita Komarov <nikoom@yandex.ru>
2017-09-28 00:06:32 +0300

    Try to fix codecov v2

--- tests/tests.py
+++ tests/tests.py
@@ -1,45 +1,40 @@
 """ Tests for Hangman """
-import unittest
+
 from Hangman import Hangman
 
 
-class TestMethods(unittest.TestCase):
-    """ Test for Hangman """
+def test_1(self):
+    """
+        Win
+    """
+    word = "Nikita"
+    game = Hangman(word, 5)
+    for letter in word:
+        game.attempt(letter)
+    game.check_status()
+    self.assertEqual(game.status, 1)
 
-    def test_1(self):
-        """
-            Win
-        """
-        word = "Nikita"
-        game = Hangman(word, 5)
-        for letter in word:
-            game.attempt(letter)
-        game.check_status()
-        self.assertEqual(game.status, 1)
+def test_2(self):
+    """
+        The game is not over
+    """
+    word = "Nikita"
+    game = Hangman(word, 5)
+    for letter in word[:-1]:
+        game.attempt(letter)
+    game.check_status()
+    self.assertEqual(game.status, 0)
 
-    def test_2(self):
-        """
-            The game is not over
-        """
-        word = "Nikita"
-        game = Hangman(word, 5)
-        for letter in word[:-1]:
-            game.attempt(letter)
-        game.check_status()
-        self.assertEqual(game.status, 0)
-
-    def test_3(self):
-        """
-            Lost
-        """
-        word = "Nikita"
-        game = Hangman(word, 5)
-        for letter in word:
-            print letter
-            game.attempt("a")
-        game.check_status()
-        self.assertEqual(game.status, -1)
+def test_3(self):
+    """
+        Lost
+    """
+    word = "Nikita"
+    game = Hangman(word, 5)
+    for letter in word:
+        print letter
+        game.attempt("a")
+    game.check_status()
+    self.assertEqual(game.status, -1)
 
 
-if __name__ == "__main__":
-    unittest.main()
